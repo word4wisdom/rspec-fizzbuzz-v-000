43bc02bdbb9e797fd0e04410dccd62c1b85904a3
Joan' Araujo <wisdombthegame@gmail.com>
2020-06-25 18:38:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -10,14 +10,3 @@ def fizzbuzz(number)
   end
 end
 
-puts('Lets play fizzbuzz')
-print('enter a number: ')
-number = gets()
-puts(number)
-if number == % 3
-  puts ('fizz')
-elsif number == % 5
-  puts ('buzz')
-elsif number == %15
-  puts ('fizzbuzz')
-end
