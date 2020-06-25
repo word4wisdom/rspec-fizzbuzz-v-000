94bb65fc4198d7ce6e66579e990f4e316a2de96b
Joan' Araujo <wisdombthegame@gmail.com>
2020-06-25 18:44:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,12 +8,3 @@ def fizzbuzz(number)
   end
 end
 
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  elsif int % 3 == 0
-    "Fizz"
-  elsif int % 5 == 0
-    "Buzz"
-  end
-end
